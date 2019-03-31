ed7a90887938554d53f33d2636c1b8f28c7a66f4
Marcio <marcio.i.silva@outlook.com>
2019-03-31 11:59:31 -0300

    acressimo de comentario
    foi colocado coemntario nos comandos

---
+++ treinamento.R
@@ -0,0 +1,80 @@
+setwd("C:/Users/marci/OneDrive/Documentos/GitHub/R")
+#setar local onde sera salvo. a barra deve ser invertida
+
+
+getwd()
+#verificar onde sera salvo
+
+contributors()
+#virifica quam ajudou na linguagem
+
+
+license()
+#verifica a licença
+
+print('ola mundo')
+#mostra menssagem
+
+sessionInfo()
+#informações da sessao
+
+
+
+plot(1:25)
+#gera grafico
+
+
+
+install.packages('randomForest')
+#instala pacotes, pode-se usar aba packages
+
+
+library(ggplot2)
+#carrega pacote
+
+
+
+
+detach(package:ggplot2)
+#descarrega pacotes
+
+
+
+
+
+help("mean")
+#chama ajuda
+
+
+?mean
+#chama juda
+
+
+install.packages("sos")
+#instalando outro tipo de ajuda
+
+
+
+library(sos)
+#carregando outro tipo de ajuda
+
+
+findFn("fread")
+#pedindo ajuda
+
+help.search('randonForest')
+#pedidno ajuda
+
+
+
+??matplot
+#pedindo ajuda
+
+
+
+example('matplot')
+#mostra exemplo de como usar
+
+
+q()
+#sair e salvar sessao
